30a3b61d800d390d44ab9697de6ba483159ca953
Florian Groß <54940184+FlorianGross@users.noreply.github.com>
2021-12-06 10:10:12 +0100

    Changes

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
       title: 'Nikolaus Punch',
       theme: ThemeData(
 
+
         primarySwatch: Colors.red,
       ),
       home: const MyHomePage(title: 'Punch the Nikolaus'),
